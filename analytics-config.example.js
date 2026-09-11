988a133440ccae5ebcbd7c452864452bc84c110f
網站小編-KAI <denny980510@gmail.com>
2026-09-11 23:49:53 +0800

    Add files via upload

---
+++ analytics-config.example.js
@@ -0,0 +1,9 @@
+window.SIGNWELL_ANALYTICS = {
+  enabled: true,
+  endpoint: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
+};
+
+window.SIGNWELL_NEWSLETTER = {
+  enabled: true,
+  endpoint: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
+};
