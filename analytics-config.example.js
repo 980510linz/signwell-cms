f6cfd1c9051f683d9a73c1278b28b74c02cab437
網站小編-KAI <denny980510@gmail.com>
2026-09-24 20:19:33 +0800

    Delete analytics-config.example.js

--- analytics-config.example.js
+++
@@ -1,9 +0,0 @@
-window.SIGNWELL_ANALYTICS = {
-  enabled: true,
-  endpoint: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
-};
-
-window.SIGNWELL_NEWSLETTER = {
-  enabled: true,
-  endpoint: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
-};
