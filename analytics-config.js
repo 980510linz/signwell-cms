9de27dcb658988b458a42fc0c172f5d7707c35b7
網站小編-KAI <denny980510@gmail.com>
2026-09-24 20:19:42 +0800

    Delete analytics-config.js

--- analytics-config.js
+++
@@ -1,9 +0,0 @@
-window.SIGNWELL_ANALYTICS = {
-  enabled: true,
-  endpoint: "https://script.google.com/macros/s/AKfycbzmZXZSepxCD1jbfpjMxsnvn0nRl-xEpeXdJoTO-TZL6Z5Zk7T-OsVGpTkIxWaCh-Y/exec"
-};
-
-window.SIGNWELL_NEWSLETTER = {
-  enabled: true,
-  endpoint: "https://script.google.com/macros/s/AKfycbzmZXZSepxCD1jbfpjMxsnvn0nRl-xEpeXdJoTO-TZL6Z5Zk7T-OsVGpTkIxWaCh-Y/exec"
-};
